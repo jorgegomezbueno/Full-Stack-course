59c979a7f1806b0ce4ab130314a2fa9fe5600fd6
jorgegomezbueno <jorge.gomez.bueno@alumnos.upm.es>
2020-09-25 19:29:09 +0100

    ButtonCounter

--- part1/src/counterIndex.js
+++ part1/src/counterIndex.js
@@ -1,15 +1,18 @@
-/*import React, { useState } from 'react'
+/*
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  setTimeout(    () => setCounter(counter + 1),    1000  )
-  return (
-    <div>{counter}</div>
-  )
+    const [ counter, setCounter ] = useState(0)
+
+    setTimeout(    () => setCounter(counter + 1),    1000  )
+
+    return (
+        <div>{counter}</div>
+    )
 }
 
 ReactDOM.render(
-  <App />, 
-  document.getElementById('root')
+    <App />, 
+    document.getElementById('root')
 )*/
